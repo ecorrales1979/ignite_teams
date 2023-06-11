13f3aadc6f4e1a9049789567113d802760646da0
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-06-11 02:13:11 -0300

    style: create theme and apply theme provider from styled-components

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components';
 
 import Groups from '@/screens/groups';
+import theme from '@/theme'
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Groups />
       <StatusBar style="auto" />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,29 @@
+export default {
+  palette: {
+    white: '#FFFFFF',
+
+    green7: '#00875F',
+    green5: '#00B37E',
+
+    red: '#F75A68',
+    redDark: '#AA2834',
+
+    gray7: '#121214',
+    gray6: '#202024',
+    gray5: '#29292E',
+    gray4: '#323238',
+    gray3: '#7C7C8A',
+    gray2: '#C4C4CC',
+    gray1: '#E1E1E6',
+  },
+  fontFamily: {
+    regular: 'Roboto_400Regular',
+    bold: 'Roboto_700Bold',
+  },
+  fontSize: {
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 24,
+  },
+};
