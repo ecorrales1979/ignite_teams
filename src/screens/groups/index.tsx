79cbb1050565794e71c31947634695c4cc752449
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-06-11 02:00:18 -0300

    feat: use styled-components

---
+++ src/screens/groups/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import { Container, Title } from './styles';
+
+export default function Groups() {
+  return (
+    <Container>
+      <Title>Groups</Title>
+    </Container>
+  );
+}
