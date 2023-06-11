bfe9bbd787b070ebdef7792b6c7dd1e3b5a69f24
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-06-11 18:24:28 -0300

    chore: add google fonts roboto

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
+import { ActivityIndicator } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import Groups from '@/screens/groups';
 import theme from '@/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? (
+        <Groups />
+      ) : (
+        <ActivityIndicator />
+      )}
       <StatusBar style="auto" />
     </ThemeProvider>
   );
