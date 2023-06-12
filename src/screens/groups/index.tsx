8da60e00a9ed4b0c4d1673f6d74a9fcf1f93cee8
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-06-11 23:56:03 -0300

    feat: create an empty list component

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -5,9 +5,10 @@ import { Container } from './styles';
 import { GroupCard } from '@/components/group-card';
 import { Header } from '@/components/header';
 import { Highlight } from '@/components/highlight';
+import { ListEmpty } from '@/components/list-empty';
 
 export default function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Group test'])
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
@@ -21,6 +22,12 @@ export default function Groups() {
         renderItem={({item}) => (
           <GroupCard title={item} />
         )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={
+          <ListEmpty
+            message="You don't have any group yet. Register your first one now!"
+          />
+        }
       />
     </Container>
   );
