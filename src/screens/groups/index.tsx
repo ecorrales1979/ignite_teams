c795598f5c9ebccce114c38a66ab271ce55d4613
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-06-11 22:50:40 -0300

    feat: add flatlist to render group cards

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,16 +1,27 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Container } from './styles';
 import { GroupCard } from '@/components/group-card';
 import { Header } from '@/components/header';
 import { Highlight } from '@/components/highlight';
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Group test'])
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Groups" subtitle="play with your team" />
 
-      <GroupCard title="Group test" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({item}) => (
+          <GroupCard title={item} />
+        )}
+      />
     </Container>
   );
 }
