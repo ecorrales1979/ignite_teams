20d9f02d9d91ba45504cf7d23ffcb4c2b7b9c38c
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-06-13 01:08:49 -0300

    feat: create the filter component

---
+++ src/components/filter/styles.ts
@@ -0,0 +1,29 @@
+import { TouchableOpacity } from 'react-native';
+import styled, { css } from 'styled-components/native';
+
+export interface FilterStyleProps {
+  isActive?: boolean;
+}
+
+export const Container = styled(TouchableOpacity)<FilterStyleProps>`
+  ${({ theme, isActive }) =>
+    isActive &&
+    css`
+      border: 1px solid ${theme.palette.green7};
+    `};
+  width: 70px;
+  height: 38px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  margin-right: 12px;
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.fontFamily.bold};
+    font-size: ${theme.fontSize.sm};
+    color: ${theme.palette.white};
+  `};
+  text-transform: uppercase;
+`;
