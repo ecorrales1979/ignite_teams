73e250dc1410f5ebc8a79ec9744a993a1b2934ee
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-06-12 22:14:25 -0300

    feat: create new group screen

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { Loading } from '@/components/loading';
 import Groups from '@/screens/groups';
+import NewGroup from '@/screens/new-group';
 import theme from '@/theme'
 
 export default function App() {
@@ -15,7 +16,8 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       {fontsLoaded ? (
-        <Groups />
+        // <Groups />
+        <NewGroup />
       ) : (
         <Loading />
       )}
---
+++ src/screens/new-group/index.tsx
@@ -0,0 +1,23 @@
+import { Container, Content, Icon } from './styles';
+import { Button } from '@/components/button';
+import { Header } from '@/components/header';
+import { Highlight } from '@/components/highlight';
+import { Input } from '@/components/input';
+
+export default function NewGroup() {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Content>
+        <Icon />
+
+        <Highlight title="New group" subtitle="Create a new group to start adding new participants" />
+
+        <Input />
+
+        <Button label="Create" style={{ marginTop: 20 }} />
+      </Content>
+    </Container>
+  )
+}
