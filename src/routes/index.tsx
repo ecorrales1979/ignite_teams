d1d39f4eb7dcde3b818aeab7dfe24282c6761cbb
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-06-18 13:48:21 -0300

    fix: prevent glitch effect when navigating between screens

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
+import { View } from 'react-native';
+import { useTheme } from 'styled-components/native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
+  const { palette } = useTheme()
+
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <View style={{ flex: 1, backgroundColor: palette.gray6 }}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
   )
 }
